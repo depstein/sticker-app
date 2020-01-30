5e9838edd709c14cc8b92f510b4ea52cb548308a
Daniel Epstein <epstein@ics.uci.edu>
2020-01-30 14:12:17 -0800

    Send a sticker from our app

--- data-stickers/src/app/send-to-snapchat/send-to-snapchat.component.ts
+++ data-stickers/src/app/send-to-snapchat/send-to-snapchat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-send-to-snapchat',
@@ -6,16 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-to-snapchat.component.scss'],
 })
 export class SendToSnapchatComponent implements OnInit {
+  @Input() sticker:string;
+  private base_url:string = "http://sheltered-waters-08469.herokuapp.com";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     //This may not work if you have an adblocker running.
-  	var js, sjs = document.getElementsByTagName('script')[0];
-  	if (document.getElementById('snapkit-creative-kit-sdk')) return;
-  	js = document.createElement('script'); js.id = 'snapkit-creative-kit-sdk';
-  	js.src = "https://sdk.snapkit.com/js/v1/create.js";
-  	sjs.parentNode.insertBefore(js, sjs);
+    var js, sjs = document.getElementsByTagName('script')[0];
+    if (document.getElementById('snapkit-creative-kit-sdk')) return;
+    js = document.createElement('script'); js.id = 'snapkit-creative-kit-sdk';
+    js.src = "https://sdk.snapkit.com/js/v1/create.js";
+    sjs.parentNode.insertBefore(js, sjs);
+  }
+
+  get URL():string {
+    return this.base_url + '/' + this.sticker;
   }
 
 }
