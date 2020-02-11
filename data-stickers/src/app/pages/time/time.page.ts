ae7f90bd281243ed57dc1e9e8b50d4f6103d5339
siyunji <siyunj1@uci.edu>
2020-02-10 21:56:46 -0800

    fix time page

--- data-stickers/src/app/pages/time/time.page.ts
+++ data-stickers/src/app/pages/time/time.page.ts
@@ -27,7 +27,25 @@ export class TimePage implements OnInit {
 	img17:any;
 	img18:any;
   constructor(private navCtrl: NavController,private router: Router) { 
-
+    this.img1 = "../../../assets/stickers/time/5-02.png";
+		this.img2 = "../../../assets/stickers/time/5-01.png";
+		this.img3 = "../../../assets/stickers/time/5-06.png" ;
+		this.img4 = "../../../assets/stickers/time/5-03.png";
+		this.img5 = "../../../assets/stickers/time/embellished-10.png";
+		this.img6 = "../../../assets/stickers/time/plain-09.png";
+		this.img7 = "../../../assets/stickers/time/5-09.png" ;
+		this.img8 = "../../../assets/stickers/time/plain-02.png";
+		this.img9 = "../../../assets/stickers/time/5-12.png" ;
+		this.img10 = "../../../assets/stickers/time/embellished-03.png";
+		this.img11 = "../../../assets/stickers/time/5-08.png";
+    this.img12 = "../../../assets/stickers/time/plain-12.png" ;
+    this.img13 = "../../../assets/stickers/time/5-07.png" ;
+	  this.img14 ="../../../assets/stickers/time/plain-09.png" ;
+	  this.img15 = "../../../assets/stickers/time/embellished-08.png" ;
+	  this.img16 = "../../../assets/stickers/time/5-02.png";
+	  this.img17 = "../../../assets/stickers/time/5-01.png" ; 
+	  this.img18 = "../../../assets/stickers/time/5-06.png" ;
+    
   }
 
   ngOnInit() {
@@ -35,7 +53,7 @@ export class TimePage implements OnInit {
 	
 	goToCreateStickerPage(this_img) {
   
-    this.router.navigate(['create-sticker', {img: this_img, domain:"heartrate"}]);
+    this.router.navigate(['create-sticker', {img: this_img, domain:"time"}]);
 	}
 
 }
