b0817018812c9666eb9ef4664e35bcff73cdfba8
rmkato <rmkato@uci.edu>
2020-03-02 17:33:01 -0800

    Added image array for each sticker page

--- data-stickers/src/app/pages/steps/steps.page.ts
+++ data-stickers/src/app/pages/steps/steps.page.ts
@@ -9,32 +9,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./steps.page.scss'],
 })
 export class StepsPage implements OnInit {
-	img1: any; 
-	img2: any; 
-	img3: any; 
-	img4: any; 
-	img5: any; 
-	img6: any; 
-	img7: any; 
-	img8: any; 
-	img9: any; 
-	img10: any; 
-	img11: any; 
-	img12: any; 	
+	imageArray = [];
 
-  constructor(private navCtrl: NavController, private router: Router) { 
-		this.img1 = "../../../assets/stickers/steps/2-02.png";
-		this.img2 = "../../../assets/stickers/steps/plain-01.png";
-		this.img3 = "../../../assets/stickers/steps/track-11.png";
-		this.img4 = "../../../assets/stickers/steps/plain-18.png";
-		this.img5 = "../../../assets/stickers/steps/rhino-28.png";
-		this.img6 = "../../../assets/stickers/steps/embellished-09.png";
-		this.img7 = "../../../assets/stickers/steps/2-12.png";
-		this.img8 = "../../../assets/stickers/steps/2-14.png";
-		this.img9 = "../../../assets/stickers/steps/plain-07.png";
-		this.img10 = "../../../assets/stickers/steps/foot.png";
-		this.img11 = "../../../assets/stickers/steps/embellished-13.png";
-		this.img12 = "../../../assets/stickers/steps/giraffe-08.png";
+  constructor(private navCtrl: NavController, private router: Router) { 		
+		this.imageArray = [
+			[
+				"../../../assets/stickers/steps/2-02.png",
+				"../../../assets/stickers/steps/plain-01.png",
+				"../../../assets/stickers/steps/track-11.png"
+			],
+			[
+				"../../../assets/stickers/steps/plain-18.png",
+				"../../../assets/stickers/steps/rhino-28.png",
+				"../../../assets/stickers/steps/embellished-09.png"
+			],
+			[
+				"../../../assets/stickers/steps/2-12.png",
+				"../../../assets/stickers/steps/2-14.png",
+				"../../../assets/stickers/steps/plain-07.png"
+			],
+			[
+				"../../../assets/stickers/steps/foot.png",
+				"../../../assets/stickers/steps/embellished-13.png",
+				"../../../assets/stickers/steps/giraffe-08.png"
+			],
+			[
+				"../../../assets/stickers/steps/2-02.png",
+				"../../../assets/stickers/steps/plain-01.png",
+				"../../../assets/stickers/steps/track-11.png"
+			],
+			[
+				"../../../assets/stickers/steps/plain-18.png",
+				"../../../assets/stickers/steps/rhino-28.png",
+				"../../../assets/stickers/steps/embellished-09.png"
+			]
+		];
+		
 	}
 
   ngOnInit() {
