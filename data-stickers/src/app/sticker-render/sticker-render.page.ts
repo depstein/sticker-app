7aafef91838bc30a4934bf601f05f38cd5978041
rmkato <rmkato@uci.edu>
2020-04-14 11:28:08 -0700

    Added ion spinner and API call to sticker rendering page

--- data-stickers/src/app/sticker-render/sticker-render.page.ts
+++ data-stickers/src/app/sticker-render/sticker-render.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { GlobalDataService } from './../global-data.service';
 import { StickerInfo } from '../sticker-info-class';
 
@@ -10,10 +11,36 @@ import { StickerInfo } from '../sticker-info-class';
   styleUrls: ['./sticker-render.page.scss'],
 })
 export class StickerRenderPage implements OnInit {
+  imageFromServer: any;
+  imageLoaded: boolean;
+  url: string;
 
-  constructor(private route: ActivatedRoute, public global: GlobalDataService) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient, public global: GlobalDataService) {
+    this.imageLoaded = false;
+    var image_arr = this.global.stickerInfo.image.split('/');
+    image_arr = image_arr[image_arr.length-1].split('.');
+
+    this.url = 'https://sheltered-waters-08469.herokuapp.com/' + String(this.global.stickerInfo.domain) + '/?value=' + String(this.global.stickerInfo.value) + '&type=' + String(image_arr[0]) + '&option=' + String(this.global.stickerInfo.animation);
+  }
 
   ngOnInit() {
+    this.http.get('https://sheltered-waters-08469.herokuapp.com/heartbeat/?value=100&type=plain-domain-relevant-2&option=shake&goal=0', {responseType: 'blob'})
+      .subscribe(data => { 
+        this.createImageFromBlob(data);
+        this.imageLoaded = true;
+      }, error => {
+        alert("error");
+      });
+  }
+
+  createImageFromBlob(image: Blob) {
+    var reader = new FileReader();
+    reader.addEventListener("load", () => {
+      this.imageFromServer = reader.result;
+    }, false)
+    if (image) {
+      reader.readAsDataURL(image);
+    }
   }
 
 	// Binding to onClick event of sharesheet button 
