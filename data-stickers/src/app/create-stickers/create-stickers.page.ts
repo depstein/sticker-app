b4576fc7beac36e3acd914956c78321d3d7ac23c
siyunji <siyunj1@uci.edu>
2020-04-22 11:55:43 -0700

    login update & social share icon

--- data-stickers/src/app/create-stickers/create-stickers.page.ts
+++ data-stickers/src/app/create-stickers/create-stickers.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { AlertController } from "@ionic/angular";
 import { ActivatedRoute, Router } from "@angular/router";
 import { GlobalDataService } from "./../global-data.service";
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: "app-create-stickers",
@@ -9,11 +11,13 @@ import { GlobalDataService } from "./../global-data.service";
   styleUrls: ["./create-stickers.page.scss"],
 })
 export class CreateStickersPage implements OnInit {
+  img_list = [];
   constructor(
     public alertController: AlertController,
     private router: Router,
     public route: ActivatedRoute,
-    public global: GlobalDataService
+    public global: GlobalDataService,
+    private storage: Storage
   ) {
     this.global.stickerInfo.image = this.route.snapshot.paramMap.get("img");
     this.global.stickerInfo.domain = this.route.snapshot.paramMap.get("domain");
@@ -25,6 +29,7 @@ export class CreateStickersPage implements OnInit {
     this.global.stickerInfo.unit = Object.keys(
       this.global.domain_info[this.global.stickerInfo.domain].units
     )[0].trim();
+    
   }
 
   ngOnInit() {}
@@ -37,9 +42,21 @@ export class CreateStickersPage implements OnInit {
   // Binding to onClick event of sharesheet button
 
   addToRecentUse() {
+   // if (!this.global.recent_use.includes(this.global.stickerInfo.image)) {
+   //   this.global.recent_use.push(this.global.stickerInfo.image);
+   // }
+   // if(this.global.recent_use.length > 3){
+   //   this.global.recent_use = this.global.recent_use.slice(1,4);
+   // }
+
     if (!this.global.recent_use.includes(this.global.stickerInfo.image)) {
       this.global.recent_use.push(this.global.stickerInfo.image);
     }
+    if(this.global.recent_use.length > 3){
+      this.global.recent_use = this.global.recent_use.slice(1,4);
+    }
+    this.img_list = this.global.recent_use;
+    this.storage.set("images", this.img_list);
   }
 
   goToStickerRenderPage() {
--- data-stickers/src/app/recent-use/recent-use.component.ts
+++ data-stickers/src/app/recent-use/recent-use.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
 import { GlobalDataService } from './../global-data.service';
-
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-recent-use',
@@ -11,11 +11,12 @@ import { GlobalDataService } from './../global-data.service';
 export class RecentUseComponent implements OnInit {
   @Input() domain;
   
-  constructor(public global: GlobalDataService, public router:Router) { }
+  constructor(public global: GlobalDataService, public router:Router,private storage: Storage) { }
 
   ngOnInit() {}
   goToCreateStickerPage(this_img) {
 		this.router.navigate(['create-sticker', {img: this_img, domain: this.domain}]);
-	}
-
+  }
+  
+  
 }
