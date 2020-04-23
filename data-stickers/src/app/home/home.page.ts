137e85b9e2ff7b6a425ea6ea54484b9ced8c240b
Daniel Epstein <epstein@ics.uci.edu>
2020-04-23 11:02:26 -0700

    Change login to be an alert

--- data-stickers/src/app/home/home.page.ts
+++ data-stickers/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  id:number;
-  constructor() {}
-  signIn(){
-
+  id:string;
+  constructor(private alertController:AlertController) {
+  	//TODO: try to load the user ID from local storage, and don't create this page if they're already logged in.
+  	this.presentLogIn();
   }
-  signUp(){
 
+  async presentLogIn() {
+  	const alert = await this.alertController.create({
+      header: 'Please Log In',
+      inputs: [
+        {
+          name: 'userId',
+          type: 'text',
+          placeholder: 'User Id'
+        }
+      ],
+      buttons: [
+      	{
+          text: 'Submit',
+          handler: (response) => {
+          	this.id = response.userId;
+          	console.log('Logged in as ' + response.userId);
+          }
+        }
+      ],
+      backdropDismiss:false
+    });
+
+    await alert.present();
   }
 }
