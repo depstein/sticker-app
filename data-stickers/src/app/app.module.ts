aff5b5c7800b1e189074e925956d168e5bc546c5
rmkato <rmkato@uci.edu>
2020-04-27 12:33:39 -0700

    Implemented Ionic local storage to store user ID

--- data-stickers/src/app/app.module.ts
+++ data-stickers/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { GlobalDataService } from "./global-data.service";
 
 import { File } from "@ionic-native/file/ngx";
 import { SocialSharing } from "@ionic-native/social-sharing/ngx";
+import { IonicStorageModule } from "@ionic/storage";
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +21,7 @@ import { SocialSharing } from "@ionic-native/social-sharing/ngx";
   imports: [
     BrowserModule,
     IonicModule.forRoot({ animated: false }),
+    IonicStorageModule.forRoot(),
     AppRoutingModule,
     HttpClientModule
   ],
--- data-stickers/src/app/home/home.page.ts
+++ data-stickers/src/app/home/home.page.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  constructor() {}
+  userId: string; 
+  idEntered: boolean;
+  idInput: string;
+  onHomeScreen: boolean;
+
+  constructor(private storage: Storage) {
+    this.idEntered = false;
+    this.onHomeScreen = true;
+    this.userId = '';
+    this.checkForUserId();
+  }
+
+  checkForUserId(){
+    this.storage.get('id')
+    .then((value) => {
+      if (value != null){
+        console.log(value);
+        this.userId = value;
+        this.idEntered = true; 
+      }
+    })
+  }
+
+  setUserId(idInput){
+    this.storage.set('id', idInput);
+    this.userId = idInput;
+    this.idEntered = true; 
+  }
+
+  clearUserId(){
+    console.log("Clearing user ID from local storage");
+    this.storage.remove('id');
+    this.userId = '';
+    this.idEntered = false;
+  }
+
+  removeButton(){
+    document.getElementById('clearButton').style.setProperty('z-index', 'auto');
+  }
+
 }
