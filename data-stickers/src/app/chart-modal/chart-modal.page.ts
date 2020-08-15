a2a16518038c578fb5dd5123d7873a2962313b13
Ryan Mohta <ryanmohta@gmail.com>
2020-08-14 20:33:53 -0700

    Added sample data for day/week/month

--- data-stickers/src/app/chart-modal/chart-modal.page.ts
+++ data-stickers/src/app/chart-modal/chart-modal.page.ts
@@ -8,8 +8,8 @@ import { ModalController } from '@ionic/angular';
 })
 export class ChartModalPage implements OnInit {
   knobValues: any;
-  // sample chart data
-  chartData: any[] = [{"t":"2020-08-07T07:00:00.000Z","y":15226.91504233753},{"t":"2020-08-08T07:00:00.000Z","y":155},{"t":"2020-08-09T07:00:00.000Z","y":2619.118505172607},{"t":"2020-08-10T07:00:00.000Z","y":16616},{"t":"2020-08-11T07:00:00.000Z","y":1021},{"t":"2020-08-12T07:00:00.000Z","y":12206},{"t":"2020-08-13T07:00:00.000Z","y":15281},{"t":"2020-08-14T07:00:00.000Z","y":10262.263678286228}];
+  chartData: any[];
+  sampleData: any;
   dataSum: number;
 
   constructor(public viewCtrl: ModalController) {
@@ -17,6 +17,7 @@ export class ChartModalPage implements OnInit {
       lower: 2,
       upper: 5
     };
+    this.chartData = [];
     this.dataSum = 0;
   }
 
@@ -29,6 +30,8 @@ export class ChartModalPage implements OnInit {
 
   onChartDataChanged(chartData: object[]) {
     this.chartData = chartData;
+    console.log(this.chartData);
+
   }
 
   getNumberOfTicks() {
