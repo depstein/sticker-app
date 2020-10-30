2421700ea41321157895716cfe307e6883948744
Ryan Mohta <ryanmohta@gmail.com>
2020-10-30 15:50:25 -0700

    Passed props into component

--- data-stickers/src/app/food-item/food-item.component.ts
+++ data-stickers/src/app/food-item/food-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-food-item',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-item.component.scss'],
 })
 export class FoodItemComponent implements OnInit {
+
+  @Input() imageUrl: string;
+  @Input() itemName: string;
+  @Input() numCalories: number;
 
   constructor() { }
 
