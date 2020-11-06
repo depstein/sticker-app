af51d1c345668f7e07987fea0a822c41dcba9cf4
Ryan Mohta <ryanmohta@gmail.com>
2020-11-06 12:43:13 -0800

    Populated counter component

--- data-stickers/src/app/counter/counter.component.ts
+++ data-stickers/src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalDataService } from "./../global-data.service";
 
 @Component({
   selector: 'app-counter',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public global: GlobalDataService) { }
 
   ngOnInit() {}
 
