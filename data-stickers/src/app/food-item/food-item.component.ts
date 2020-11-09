9f6c82bf2b1b8349c2c4459ff78d3c2ba07e2d99
Ryan Mohta <ryanmohta@gmail.com>
2020-11-09 04:01:41 -0800

    Updated counter

--- data-stickers/src/app/food-item/food-item.component.ts
+++ data-stickers/src/app/food-item/food-item.component.ts
@@ -10,9 +10,16 @@ export class FoodItemComponent implements OnInit {
   @Input() imageUrl: string;
   @Input() itemName: string;
   @Input() numCalories: number;
+  @Input() totalCalories: number;
+  @Output() totalCaloriesChanged = new EventEmitter<number>(true);
 
   constructor() { }
 
   ngOnInit() {}
+
+  onCounterValueChanged(counterValue: number) {
+    // this.totalCaloriesChanged.emit(this.numCalories * counterValue);
+    this.totalCalories = this.numCalories * counterValue;
+  }
 
 }
