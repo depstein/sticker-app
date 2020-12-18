9f48e4fc8f5bbd83d2924e262897d6624850c18b
Ryan Mohta <ryanmohta@gmail.com>
2020-12-18 12:58:59 -0800

    Created get request in service

--- data-stickers/src/app/food-data.service.ts
+++ data-stickers/src/app/food-data.service.ts
@@ -9,4 +9,21 @@ import { catchError, retry } from 'rxjs/operators';
 export class FoodDataService {
 
   constructor(private http: HttpClient) { }
+
+  getFoodData() {
+    return this.http.get(
+      'https://trackapi.nutritionix.com/v2/natural/nutrients',
+      options: {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-app-id': '67853ab2',
+          'x-app-key': 'd27556fd8ab42acd7a7def7d91347f0e',
+          'x-remote-user-id': '0'
+        },
+        params: {
+          "query":"chicken nachos and milk"
+        }
+      }
+    );
+  }
 }
