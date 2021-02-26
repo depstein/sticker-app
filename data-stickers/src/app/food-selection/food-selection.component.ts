f6a473078d652b97628e1de630f6bda09be79399
Ryan Mohta <ryanmohta@gmail.com>
2021-02-25 22:20:18 -0800

    Updated nutrients objects

--- data-stickers/src/app/food-selection/food-selection.component.ts
+++ data-stickers/src/app/food-selection/food-selection.component.ts
@@ -33,14 +33,11 @@ export class FoodSelectionComponent {
           name: item.food_name,
           image: item.photo.thumb,
           nutrients: {
-            calories: 0,
-            cholesterol: 0,
-            fiber: 0,
-            potassium: 0,
-            fat: 0,
-            carbohydrate: 0,
-            sodium: 0,
-            sugar: 0
+            "calories": 0,
+            "g fiber": 0,
+            "g carbohydrate": 0,
+            "g sodium": 0,
+            "g sugar": 0
           }
         }
       });
@@ -50,42 +47,16 @@ export class FoodSelectionComponent {
         .then((nutritionData: any) => {
           console.log(nutritionData);
 
-// nf_calories: 94.64
-
-// nf_cholesterol: 0
-//
-// nf_dietary_fiber: 4.37
-//
-// nf_p: 20.02
-//
-// nf_potassium: 194.74
-//
-// nf_protein: 0.47
-//
-// nf_saturated_fat: 0.05
-//
-// nf_sodium: 1.82
-//
-// nf_sugars: 18.91
-//
-// nf_total_carbohydrate: 25.13
-//
-// nf_total_fat: 0.31
-
           return {
             name: item.food_name,
             image: item.photo.thumb,
             nutrients: {
-              calories: Math.round(nutritionData.foods[0].nf_calories),
-              cholesterol: nutritionData.foods[0].nf_cholesterol,
-              fiber: nutritionData.foods[0].nf_dietary_fiber,
-              potassium: nutritionData.foods[0].nf_potassium,
-              fat: nutritionData.foods[0].nf_total_fat,
-              carbohydrate: nutritionData.foods[0].nf_total_carbohydrate,
-              sodium: nutritionData.foods[0].nf_sodium,
-              sugar: nutritionData.foods[0].nf_sugars
+              "calories": Math.round(nutritionData.foods[0].nf_calories),
+              "g fiber": nutritionData.foods[0].nf_dietary_fiber,
+              "g carbohydrate": nutritionData.foods[0].nf_total_carbohydrate,
+              "g sodium": nutritionData.foods[0].nf_sodium,
+              "g sugar": nutritionData.foods[0].nf_sugars
             }
-            // calories: Math.round(nutritionData.foods[0].nf_calories)
           };
         });
       });
